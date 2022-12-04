8970acfc200c4490f6761325566b20f4d61d1291
Greg Puccio <gregpuccio@gmail.com>
2022-12-04 17:51:36 -0500

    continued giving options for check in

--- lib/src/views/home/home_page.dart
+++ lib/src/views/home/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lhs_fencing/src/constants/lists.dart';
 import 'package:lhs_fencing/src/models/attendance.dart';
 import 'package:lhs_fencing/src/models/user_data.dart';
 import 'package:lhs_fencing/src/services/auth/auth_service.dart';
@@ -19,13 +21,20 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
-  String formattedDate = DateFormat('EEEE, MMMM d').format(DateTime.now());
   int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     Widget whenData(UserData? userData) {
       if (userData != null) {
+        DateTime now = DateTime.now();
+        final closetsDateTimeToNow = possibleAttendances.reduce((a, b) =>
+            a.difference(now).abs() < b.difference(now).abs() ? a : b);
+        String formattedDate =
+            DateFormat('EEEE, MMMM d @ h:mm aa').format(closetsDateTimeToNow);
+        List<DateTime> attendances = possibleAttendances.where((date) {
+          return date.isBefore(DateTime.now());
+        }).toList();
         return Scaffold(
           appBar: const DefaultAppBar(),
           body: StreamBuilder<List<Attendance>>(
@@ -33,7 +42,7 @@ class _HomePageState extends ConsumerState<HomePage> {
               builder: (context, snapshot) {
                 return ListView.separated(
                   padding: const EdgeInsets.all(8.0),
-                  itemCount: 10,
+                  itemCount: attendances.length + 2,
                   itemBuilder: (context, index) {
                     if (index == 0) {
                       return Column(
@@ -48,12 +57,61 @@ class _HomePageState extends ConsumerState<HomePage> {
                           const Divider(),
                           SizedBox(
                             width: MediaQuery.of(context).size.width,
-                            child: const TextBadge(text: "Today"),
+                            child: const TextBadge(text: "Today's Attendance"),
                           ),
                           ListTile(
-                            title: Text("Attendance: $formattedDate"),
+                            title: Text(formattedDate),
                             trailing: ElevatedButton.icon(
-                              onPressed: () {},
+                              onPressed: () {
+                                DateTime now = DateTime.now();
+                                final closetsDateTimeToNow = possibleAttendances
+                                    .reduce((a, b) => a.difference(now).abs() <
+                                            b.difference(now).abs()
+                                        ? a
+                                        : b);
+
+                                if (now.isBefore(closetsDateTimeToNow
+                                        .add(const Duration(hours: 2))) &&
+                                    now.isAfter(closetsDateTimeToNow.subtract(
+                                        const Duration(minutes: 15)))) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      title: const Text("Check In To Practice"),
+                                      content: const Text(
+                                          "Please only check in when you have arrived at the gym. Have you arrived at the fencing gym?"),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () => context.popRoute(),
+                                          child: const Text("No, cancel"),
+                                        ),
+                                        TextButton(
+                                          onPressed: () {},
+                                          child: const Text("Yes, check in"),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                } else if (now.isBefore(closetsDateTimeToNow
+                                    .subtract(const Duration(minutes: 15)))) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      title:
+                                          const Text("Too Early For Check In"),
+                                      content: const Text(
+                                          "You cannot check in for attendance before practice has started. Make sure it is time for practice before you try checking in again."),
+                                      actions: [
+                                        TextButton(
+                                            onPressed: () => context.popRoute(),
+                                            child: const Text(
+                                              "Understood",
+                                            ))
+                                      ],
+                                    ),
+                                  );
+                                }
+                              },
                               icon: const Icon(Icons.check),
                               label: const Text("Check In"),
                             ),
@@ -65,7 +123,7 @@ class _HomePageState extends ConsumerState<HomePage> {
                           ),
                         ],
                       );
-                    } else if (index == 10 - 1) {
+                    } else if (index == attendances.length + 1) {
                       return OutlinedButton.icon(
                         onPressed: () => AuthService().signOut(),
                         icon: const Text("Sign Out"),
@@ -73,8 +131,11 @@ class _HomePageState extends ConsumerState<HomePage> {
                       );
                     } else {
                       index--;
+                      DateTime date = attendances.reversed.toList()[index];
+                      String formattedDate =
+                          DateFormat('EEEE, MMMM d @ h:mm aa').format(date);
                       return ListTile(
-                        title: Text("Attendance: $formattedDate"),
+                        title: Text(formattedDate),
                         subtitle: const Text("Checked in: 6:03 PM"),
                       );
                     }
