07a9180c31578003ad1edb48fe5ac4902fe20e26
Greg Puccio <gregpuccio@gmail.com>
2022-12-04 11:32:59 -0500

    fixed logo

--- lib/src/constants/widgets/livingston_logo.dart
+++ lib/src/constants/widgets/livingston_logo.dart
@@ -1,4 +1,3 @@
 import 'package:flutter/material.dart';
 
-Image livingstonLogo = Image.network(
-    "https://www.livingston.org/cms/lib/NJ01000562/Centricity/Template/GlobalAssets/images/logos/DistrictLogo.png");
+Image livingstonLogo = Image.asset("assets/LivingstonLogo.png");
