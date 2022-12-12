3acfd7a49a78b8556080fde4be71f7fa287b9937
Greg Puccio <gregpuccio@gmail.com>
2022-12-11 21:18:57 -0500

    test for email sending if fencers arent present

--- lib/src/views/admin/practice_page.dart
+++ lib/src/views/admin/practice_page.dart
@@ -131,6 +131,10 @@ class PracticePage extends ConsumerWidget {
                                         query:
                                             "bcc=${List.generate(absentFencers.length, (index) => absentFencers[index].email).join(",")}&subject=Absent from practice ${practice.startString}&body=Hello,\nOur records are showing that you were not at practice ${practice.startString}.\nIf you have not already provided a reason, please add a comment on the attendance site ASAP.\nThank you,\nCoach ${coach.firstName}");
                                     try {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                              content: Text(
+                                                  "Trying to launch: $url")));
                                       // if (await canLaunchUrl(url)) {
                                       launchUrl(
                                         url,
